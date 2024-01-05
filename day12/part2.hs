2837a1208424e1319ecbd2998ae240e67f22afd0
Kieran Grant <kierang92@gmail.com>
2024-01-05 13:05:51 +0000

    part 2 (tidy)

--- day12/part2.hs
+++ day12/part2.hs
@@ -3,10 +3,10 @@ import Data.MemoTrie (memo2)
 
 main :: IO ()
 main = do
-    content <- readFile "input.txt"
-    let ls = lines content
-    let soln = map getValids ls
-    print $ sum soln
+  content <- readFile "input.txt"
+  let ls = lines content
+  let soln = map getValids ls
+  print $ sum soln
 
 parse :: String -> (String, [Int])
 parse str = (head wds, toIntList $ last wds)
@@ -30,13 +30,13 @@ countWays = memo2 countWays'
     countWays' [] _ = 0
     countWays' xs [] = if '#' `elem` xs then 0 else 1
     countWays' (c : cs) (n : ns) =
-        if length (c : cs) < sum (n : ns) + length (n : ns) - 1 -- can't fit run
-            then 0
-            else case c of
-                '.' -> countWays cs (n : ns)
-                '?' -> countWays ('.' : cs) (n : ns) + countWays ('#' : cs) (n : ns)
-                '#' ->
-                    if ('.' `elem` take (n - 1) cs) || ((n - 1 < length cs) && (cs !! (n - 1) == '#'))
-                        then 0
-                        else countWays (drop n cs) ns
-                _ -> error "Not a recognised character!"
+      if length (c : cs) < sum (n : ns) + length (n : ns) - 1 -- can't fit run
+        then 0
+        else case c of
+          '.' -> countWays cs (n : ns)
+          '?' -> countWays ('.' : cs) (n : ns) + countWays ('#' : cs) (n : ns)
+          '#' ->
+            if ('.' `elem` take (n - 1) cs) || ((n - 1 < length cs) && (cs !! (n - 1) == '#'))
+              then 0
+              else countWays (drop n cs) ns
+          _ -> error "Not a recognised character!"
