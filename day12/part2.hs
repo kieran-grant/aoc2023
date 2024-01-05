517bc410791b560a8dc5037afd2befccb1dabaf1
kieran-grant <kierang92@gmail.com>
2024-01-05 12:56:15 +0000

    day 12 - part 2 (memotrie memoization)

--- day12/part2.hs
+++ day12/part2.hs
@@ -1,13 +1,12 @@
-import Data.Function.Memoize (memoize2)
 import Data.List.Split (splitOn)
+import Data.MemoTrie (memo2)
 
 main :: IO ()
 main = do
-  content <- readFile "input.txt"
-  let ls = lines content
-  let soln = map getValids ls
-  print soln
-  print $ sum soln
+    content <- readFile "input.txt"
+    let ls = lines content
+    let soln = map getValids ls
+    print $ sum soln
 
 parse :: String -> (String, [Int])
 parse str = (head wds, toIntList $ last wds)
@@ -25,19 +24,19 @@ getValids str = countWays repeatedRecords repeatedInts
     repeatedInts = concat $ replicate 5 ints
 
 countWays :: String -> [Int] -> Int
-countWays = memoize2 countWays'
+countWays = memo2 countWays'
   where
     countWays' [] [] = 1
     countWays' [] _ = 0
     countWays' xs [] = if '#' `elem` xs then 0 else 1
-    countWays' k@(c : cs) v@(n : ns) =
-      if length (c : cs) < sum (n : ns) + length (n : ns) - 1 -- can't fit run
-        then 0
-        else case c of
-          '.' -> countWays cs (n : ns)
-          '?' -> countWays ('.' : cs) (n : ns) + countWays ('#' : cs) (n : ns)
-          '#' ->
-            if ('.' `elem` take (n - 1) cs) || ((n - 1 < length cs) && (cs !! (n - 1) == '#'))
-              then 0
-              else countWays (drop n cs) ns
-          _ -> error "Not a recognised character!"
+    countWays' (c : cs) (n : ns) =
+        if length (c : cs) < sum (n : ns) + length (n : ns) - 1 -- can't fit run
+            then 0
+            else case c of
+                '.' -> countWays cs (n : ns)
+                '?' -> countWays ('.' : cs) (n : ns) + countWays ('#' : cs) (n : ns)
+                '#' ->
+                    if ('.' `elem` take (n - 1) cs) || ((n - 1 < length cs) && (cs !! (n - 1) == '#'))
+                        then 0
+                        else countWays (drop n cs) ns
+                _ -> error "Not a recognised character!"
