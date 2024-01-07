a4f0090abafe27c8dd8d4ef3d5c3284c1bc282a2
Kieran Grant <kierang92@gmail.com>
2024-01-07 13:50:04 +0000

    day 14 part 2 cycle detection sample working

--- day14/part2.hs
+++ day14/part2.hs
@@ -8,8 +8,42 @@ main :: IO ()
 main = do
   content <- readFile "sample.txt"
   let ls = lines content
-  let soln = hammer spinCycle ls
-  mapM_ print soln
+  -- let soln = hammer spinCycle ls
+  let loads = map totalNorthLoad (take 50 $ iterate spinCycle ls)
+  print loads
+
+  -- let (st, cy) = findCycle (drop 4 loads)
+  -- print st
+  -- print cy
+
+  -- let startIndex = 1 + length st
+  -- let cycleLength = length cy
+  -- let idx = billionth = ()
+
+  let initial = 3
+  let cycleLength = 7
+  let idx = ((1000000000 - initial) `mod` cycleLength) + initial
+  let billionth = totalNorthLoad (iterate spinCycle ls !! idx)
+  print idx
+  print billionth
+
+--
+--
+-- mapM_ print soln
+
+findCycle :: (Eq a) => [a] -> ([a], [a])
+findCycle xxs = fCycle xxs xxs
+  where
+    fCycle (x : xs) (_ : y : ys)
+      | x == y = fStart xxs xs
+      | otherwise = fCycle xs ys
+    fCycle _ _ = (xxs, []) -- not cyclic
+    fStart (x : xs) (y : ys)
+      | x == y = ([], x : fLength x xs)
+      | otherwise = let (as, bs) = fStart xs ys in (x : as, bs)
+    fLength x (y : ys)
+      | x == y = []
+      | otherwise = y : fLength x ys
 
 totalNorthLoad :: [[Char]] -> Int
 totalNorthLoad grid = sum $ map totalLoad (rotR grid)
