c052bc70c3442ad1883b430325a1f69725648d5d
kieran-grant <kierang92@gmail.com>
2024-01-10 12:14:46 +0000

    day 16 - part 1 scratch

--- day16/part1.hs
+++ day16/part1.hs
@@ -1,5 +1,6 @@
 import Data.Array.Unboxed
 import Data.List (nub)
+import Data.MemoTrie (memo2)
 
 data Direction = North | East | South | West deriving (Show, Eq)
 
@@ -9,41 +10,31 @@ type CharArray = UArray Coord Char
 
 main :: IO ()
 main = do
-    contents <- readFile "sample.txt"
+    contents <- readFile "input.txt"
     let cArr = toArray $ lines contents
-    let energized = buildPath cArr (0, 0) East
-    let stub = take 1000 energized
-    let oneIndexed = [((x + 1, y + 1), dir) | ((x, y), dir) <- stub]
-    let (st, cy) = findCycle oneIndexed
+    let energized = buildPath cArr [] (0, 0) East
+    (print . length . nub) energized
 
-    print $ nub oneIndexed
-    print st
-    print cy
-
-buildPath :: CharArray -> Coord -> Direction -> [(Coord, Direction)]
-buildPath cArr curr dir =
+buildPath :: CharArray -> [(Coord, Direction)] -> Coord -> Direction -> [Coord] -- alter this function to maintain a list of todo states, do the todo at the end
+buildPath cArr visited curr dir =
     let next = move curr dir
-     in if outOfBounds cArr next
-            then []
+        newVisited = (curr, dir) : visited
+        f = buildPath cArr newVisited next
+     in if outOfBounds cArr next || ((curr, dir) `elem` visited)
+            then curr : [x | (x, _) <- visited]
             else case (cArr ! next, dir) of
-                ('/', _) -> (curr, dir) : buildPath cArr next (turnR dir)
-                ('\\', _) -> (curr, dir) : buildPath cArr next (turnL dir)
-                ('-', North) -> (curr, dir) : horizontalSplit cArr next
-                ('-', South) -> (curr, dir) : horizontalSplit cArr next
-                ('|', East) -> (curr, dir) : verticalSplit cArr next
-                ('|', West) -> (curr, dir) : verticalSplit cArr next
-                _ -> (curr, dir) : buildPath cArr next dir
+                ('/', _) -> f (turnR dir)
+                ('\\', _) -> f (turnL dir)
+                ('-', North) -> f West ++ f East
+                ('-', South) -> f West ++ f East
+                ('|', East) -> f North ++ f South
+                ('|', West) -> f North ++ f South
+                _ -> f dir
 
 outOfBounds :: CharArray -> Coord -> Bool -- check if move would go out of bounds
 outOfBounds cArr (row, col) =
     let (_, (rowBounds, colBounds)) = bounds cArr
      in row < 0 || row > rowBounds || col < 0 || col > colBounds
-
-horizontalSplit :: CharArray -> Coord -> [(Coord, Direction)] -- buildPath ... ++ buildPath ...
-horizontalSplit cArr next = buildPath cArr next West ++ buildPath cArr next East
-
-verticalSplit :: CharArray -> Coord -> [(Coord, Direction)] -- buildPath ... ++ buildPath ...
-verticalSplit cArr next = buildPath cArr next South ++ buildPath cArr next North
 
 move :: Coord -> Direction -> Coord
 move (row, col) dir = case dir of
@@ -72,18 +63,3 @@ toArray lists =
             (x : _) -> length x
         bnds = ((0, 0), (rows - 1, cols - 1))
      in listArray bnds $ concat lists
-
--- Floyd's cycle detection algorithm (https://wiki.haskell.org/Floyd%27s_cycle-finding_algorithm)
-findCycle :: (Eq a) => [a] -> ([a], [a])
-findCycle xxs = fCycle xxs xxs
-  where
-    fCycle (x : xs) (_ : y : ys)
-        | x == y = fStart xxs xs
-        | otherwise = fCycle xs ys
-    fCycle _ _ = (xxs, []) -- not cyclic
-    fStart (x : xs) (y : ys)
-        | x == y = ([], x : fLength x xs)
-        | otherwise = let (as, bs) = fStart xs ys in (x : as, bs)
-    fLength x (y : ys)
-        | x == y = []
-        | otherwise = y : fLength x ys
